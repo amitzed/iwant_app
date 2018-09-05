50552adde671a0e22c9b84033179133845010402
Amit <amit@AZ-iMac.local>
2018-09-05 16:36:44 -0400

    Futher Heroku deploy troubleshooting

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// server.js
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname + "/dist")));
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('server started '+ port);
