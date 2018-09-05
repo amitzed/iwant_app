06e1710f00a14b04e46362e6bca2be2f2ee86548
Amit <amit@AZ-iMac.local>
2018-09-05 01:25:11 -0400

    created Car Page and somestyling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/iwlogo.png">
+    <Cars />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cars from './components/Cars.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cars
   }
 }
 </script>
+@import url('https://fonts.googleapis.com/css?family=Rubik|VT323|Yatra+One" rel="stylesheet')
 
 <style>
+
+body {
+  background-color: #F2E4B1;
+  /* font-family: 'VT323', monospace; */
+  /* font-family: 'Rubik', sans-serif;
+  font-family: 'Yatra One', cursive; */
+  display: grid;
+  margin: 35px;
+  height: 100%;
+  justify-items: center;
+}
+
+html {
+margin: 35px;
+height: 100%;
+}
+
+img {
+  background: linear-gradient(270deg, #58B7B8, #DF6127, #12403E);
+  border-radius: 50%;
+  animation-name: size-in;
+  animation-duration: 2s;
+  animation-delay: .1s;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +52,32 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+/* Animations */
+@keyframes size-in {
+    from {
+      transform: scale(.1);
+    }
+    to {
+      transform: scale(1);
+    }
+}
+
+@keyframes background-move {
+     0%{background-position:0% 50%}
+     50%{background-position:100% 50%}
+     100%{background-position:0% 50%}
+ }
+
+ @keyframes spin {
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
+}
+
+
 </style>
---
+++ src/components/Cars.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <div class="listName">
+
+
+      <p>List The Cars You Want</p>
+      <ul>
+        <li v-for="(data, index) in cars" :key='index'>
+          {{ data.car }}
+        </li>
+      </ul>
+      <!-- <p v-if="cars.length >= 1">You entered more than one car</p>
+      <p v-else="cars.length >= 1">You entered less than one car</p> -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cars',
+  data() {
+    return {
+      car: '',
+      cars: [
+        { "car": "Lamborghini Countach"},
+        { "car": "BMW M8"},
+        { "car": "Bugatti Chiron"},
+        { "car": "Mercedes-Benz AMG GT"}
+      ]
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.listName {
+  background: #12403E;
+  color: #F8CE3D;
+}
+
+p {
+  font-family: 'Rubik', sans-serif;
+  font-size: 1.4em;
+  padding-bottom: 10px;
+  box-shadow: 25px 10px #DF6127, -25px -15px #58B7B8;
+  padding-top: 20px;
+}
+
+ul, li {
+  font-family: 'VT323', monospace;
+  font-size: 1.1em;
+  font-weight: bold;
+  background-color: #F8CE3D;
+  border: 5px solid #58B7B8;
+  margin-bottom: 5px;
+  padding: 10px;
+  color: #12403E;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+/* input {
+  width: calc(100% - 40px);
+} */
+
+</style>
